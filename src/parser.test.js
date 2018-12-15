37f539d428d3400b4c86cd84368d2b5ccdd0690e
Felix Tripier <ftripier@gmail.com>
2018-12-15 08:27:25 -0800

    More linting
    More sophisticated tests

--- src/parser.test.js
+++ src/parser.test.js
@@ -1,7 +1,15 @@
-import grammar from "./grammar.ohm";
+import ohm from 'ohm-js';
+import grammar from './grammar.ohm';
 
-describe("grammar string", () => {
-  it("should exist", () => {
+describe('grammar string', () => {
+  it('should exist', () => {
     expect(grammar).toBeTruthy();
+  });
+});
+
+describe('grammar parser', () => {
+  it('should match an expression correctly', () => {
+    const parser = ohm.grammar(grammar);
+    expect(parser.match('1 + 1').succeeded()).toBeTruthy();
   });
 });
--- testing/fileTransformer.js
+++ testing/fileTransformer.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = {
-  process(src, filename, config, options) {
+  process(src, filename) {
     const source = fs.readFileSync(filename, 'utf8');
     return `module.exports = ${JSON.stringify(source)};`;
   }
