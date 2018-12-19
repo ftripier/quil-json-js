8988385b4e4252ccdec7e6a31385c2a02308052f
Felix Tripier <ftripier@gmail.com>
2018-12-19 09:50:47 -0800

    Building properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
-module.exports = {
+const path = require('path');
+
+const baseConfig = {
+  entry: './src/quilToJSON.js',
   module: {
-    loaders: [
-      { exclude: ['node_modules'], loader: 'babel', test: /\.js$/ },
+    rules: [
+      { exclude: [path.resolve(__dirname, 'node_modules')], loader: 'babel-loader', test: /\.js$/ },
       { loader: 'raw-loader', test: /\.ohm$/ }
     ]
-  },
-  resolve: {
-    extensions: ['', 'js'],
-    modules: ['node_modules']
   }
 };
+
+const browserConfig = {
+  ...baseConfig,
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'browser-main.js'
+  }
+};
+
+const nodeConfig = {
+  ...baseConfig,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'node-main.js'
+  }
+};
+
+module.exports = [browserConfig, nodeConfig];
